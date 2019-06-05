166b03f2d60635342cb9fd899b65633f9d78e48f
stafford <stafford.williams@gmail.com>
2019-06-06 07:57:41 +1000

    initial commit

---
+++ src/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace satire
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
